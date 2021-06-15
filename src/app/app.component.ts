f6285013da2d7de3497548575fe680e17515bf92
MethmaW <methma.cw@gmail.com>
2021-06-15 17:53:45 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'pipes';
+	name: string = "";
+	paymentDate: string = "";
+	amount!: number;
+	height!: number;
+	miles!: number;
+
+	onNameChange(value: string) {
+		this.name = value;
+	}
+
+	onDateChange(value: string) {
+		this.paymentDate = value;
+	}
+
+	onAmountChange(value: string) {
+		this.amount = parseFloat(value);
+	}
+
+	onHeightChange(value: string) {
+		this.height = parseFloat(value);
+	}
+
+	onMileChange(value: string) {
+		this.miles = parseFloat(value);
+	}
 }
